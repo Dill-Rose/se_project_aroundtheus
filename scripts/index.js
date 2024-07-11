d05c4576f18aed8299f9aa7f5a87378cb5e5fae0
DillonRosenberger <drosey15@gmail.com>
2024-07-10 19:12:26 -0700

    Add JS code for tasks 1 and 2

--- scripts/index.js
+++ scripts/index.js
@@ -26,15 +26,33 @@ const initialCards = [
 ];
 
 let modalEdit = document.querySelector("#profile-edit-button");
-let profileModal = document.querySelector("#profile-edit-modal");
+let profileEditModal = document.querySelector("#profile-edit-modal");
 let modalClose = document.querySelector("#modal-close-button");
+let profileTitle = document.querySelector(".profile__title");
+let profileDescription = document.querySelector(".profile__description");
+let profileTitleInput = document.querySelector("#profile-title-input");
+let profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+let profileEditForm = profileEditModal.querySelector(".modal__form");
 
 function openModal() {
-  profileModal.classList.add("modal_opened");
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal_opened");
 }
 modalEdit.addEventListener("click", openModal);
 
 function closeModal() {
-  profileModal.classList.remove("modal_opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 modalClose.addEventListener("click", closeModal);
+
+function handleProfileSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closeModal();
+}
+
+profileEditForm.addEventListener("submit", handleProfileSubmit);
