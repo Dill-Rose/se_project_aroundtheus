aab19f991a0b35b2f590b5a05b3f9ec50aeaf86e
DillonRosenberger <drosey15@gmail.com>
2024-11-06 07:41:55 -0700

    Fix submission errors part 2

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -39,8 +39,7 @@ export default class FormValidator {
     });
 
     if (foundInvalid) {
-      this._submitButton.classList.add(this._inactiveButtonClass);
-      this._submitButton.disabled = true;
+      this.disableSubmitButton();
     } else {
       this._submitButton.classList.remove(this._inactiveButtonClass);
       this._submitButton.disabled = false;
